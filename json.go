4aa975517f9f6b63337eeca210a04a064975a210
Chase Geigle <geigle1@illinois.edu>
2017-01-18 21:11:55 -0600

    Add fractional seconds support to date/time tests.
    The test harness is careful to ensure that only millisecond precision
    is required, as this is the minimum requirement specified by the
    standard.

--- json.go
+++ json.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"strconv"
+	"time"
+)
+
+const (
+	RFC3339Milli     = "2006-01-02T15:04:05.999Z07:00"
+	RFC3339Local     = "2006-01-02T15:04:05.999"
+	RFC3339LocalTime = "15:04:05.999"
 )
 
 // compareJson consumes the recursive structure of both `expected` and `test`
@@ -119,18 +126,35 @@ func (r result) cmpJsonValues(e, t map[string]interface{}) result {
 	// Floats need special attention too. Not every language can
 	// represent the same floats, and sometimes the string version of
 	// a float can be wonky with extra zeroes and what not.
-	if etype == "float" {
-		enum, ok := e["value"].(string)
+	//
+	// Similarly, datetimes and times need special attention because the
+	// standard only requires millisecond precision, but parsers may output
+	// more than millisecond resolution in the fractional seconds.
+	if etype == "float" || etype == "datetime" || etype == "local_time" || etype == "local_datetime" {
+		estr, ok := e["value"].(string)
 		if !ok {
 			return r.failedf("BUG in test case. 'value' should be a string, "+
 				"but it is a %T.", e["value"])
 		}
-		tnum, ok := t["value"].(string)
+		tstr, ok := t["value"].(string)
 		if !ok {
 			return r.failedf("Malformed parser output. 'value' should be a "+
 				"string but it is a %T.", t["value"])
 		}
-		return r.cmpFloats(enum, tnum)
+
+		if etype == "float" {
+			return r.cmpFloats(estr, tstr)
+		} else {
+			var dtype string
+			if etype == "datetime" {
+				dtype = RFC3339Milli
+			} else if etype == "local_datetime" {
+				dtype = RFC3339Local
+			} else {
+				dtype = RFC3339LocalTime
+			}
+			return r.cmpDateTime(dtype, estr, tstr)
+		}
 	}
 
 	// Otherwise, we can do simple string equality.
@@ -156,6 +180,30 @@ func (r result) cmpFloats(e, t string) result {
 	if ef != tf {
 		return r.failedf("Values for key '%s' don't match. Expected a "+
 			"value of '%v' but got '%v'.", r.key, ef, tf)
+	}
+	return r
+}
+
+func (r result) cmpDateTime(dtype, e, t string) result {
+	edate, err := time.Parse(dtype, e)
+	if err != nil {
+		return r.failedf("BUG in test case. Could not read '%s' as a Time "+
+			"value for key '%s'.", e, r.key)
+	}
+
+	tdate, err := time.Parse(dtype, t)
+	if err != nil {
+		return r.failedf("Malformed parser output. Could not read '%s' as "+
+			"a Time value for key '%s'.", t, r.key)
+	}
+
+	// compare only up to millisecond resolution as required by the
+	// standard
+	estr := edate.Format(dtype)
+	tstr := tdate.Format(dtype)
+	if estr != tstr {
+		return r.failedf("Values for key '%s' don't match. Expected a "+
+			"value of '%s' but got '%v'.", r.key, estr, tstr)
 	}
 	return r
 }
